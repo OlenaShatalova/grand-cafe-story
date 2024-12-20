764a5c38021e1c09a069dbf925953d393f21ae8a
Olena Shatalova <08082023m@gmail.com>
2024-12-20 15:44:50 +0200

    add gallery-swiper

---
+++ src/js/gallery.js
@@ -0,0 +1,38 @@
+import Swiper from 'swiper';
+import { Navigation, Keyboard } from 'swiper/modules';
+
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+
+const gallerySwiper = new Swiper('.swiper', {
+  modules: [Navigation, Keyboard],
+
+  navigation: {
+    nextEl: '.gallery-button-next',
+    prevEl: '.gallery-button-prev',
+  },
+
+  direction: 'horizontal',
+  autoHeight: false,
+  autoWidth: false,
+  watchOverflow: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 82,
+    },
+  },
+});
+
+gallerySwiper.update();
