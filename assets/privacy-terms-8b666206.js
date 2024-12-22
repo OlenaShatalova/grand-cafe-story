ab0de5ccd0b01470b48012cf9831ef00a18421a9
OlenaShatalova <164022674+OlenaShatalova@users.noreply.github.com>
2024-12-22 15:07:20 +0000

    Deploying to gh-pages from @ OlenaShatalova/grand-cafe-story@fd11539d67f7c4fddffe0b871a82b4f0c1465c4e 🚀

---
+++ assets/privacy-terms-8b666206.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".privacy-terms-header"),s=e.offsetTop-24;window.addEventListener("scroll",()=>{window.scrollY>=s?e.classList.add("sticky"):e.classList.remove("sticky")});
+//# sourceMappingURL=privacy-terms-8b666206.js.map
